3a390ba0efe1c44e6dc8c9e795d741960033ac7a
darshy <dxrshx101@gmail.com>
2023-11-23 01:50:54 +0800

    new

--- clinic_web_dev/public_html/login.php
+++ clinic_web_dev/public_html/login.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
-if (isset($_POST['signin'])) {
+if (isset($_POST['signin'])) { 
     if (!empty($_POST['csrf_token']) && hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
         $dbc = mysqli_connect("localhost", "root", "");
         mysqli_select_db($dbc, "clinic_reservation");
